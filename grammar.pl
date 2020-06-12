e009296d3824f9e201351ad7181d9c1d4c14e277
OswaldoRamirezT <jose_ramirez_2498@hotmail.com>
2020-06-12 13:56:04 -0500

    Ejercicio Grammar

--- grammar.pl
+++ grammar.pl
@@ -1,6 +1,6 @@
 %
 %	Gramatica
-%
+%	Ramirez Aguilar Jose Oswaldo, Sanchez Avila Gustavo
 %	<expr> ::= <op> <numero> <numero>
 %	<op> ::= '-' | '+' | '/' | '*'
 %	<numero> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
